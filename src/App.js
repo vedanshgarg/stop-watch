5ca7a1030be83971bf3eb8750691bbc6434fc42e
vedanshgarg <vedansh.garg.7@gmail.com>
2020-06-22 00:14:55 +0530

    for loop same thread

--- src/App.js
+++ src/App.js
@@ -1,110 +1,112 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Display from "./components/Display/Display";
 import WebWorkerDemo from "./components/WebWorkerDemo/WebWorkerDemo";
 
-class App extends React.Component{
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeInDeciSeconds: 0,
+      timeInSeconds: 0,
+      timeInMinutes: 0,
+      timerRunning: false,
+      initiateReset: false,
+      timerInterval: 0,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            timeInDeciSeconds: 0,
-            timeInSeconds: 0,
-            timeInMinutes: 0,
-            timerRunning: false,
-            initiateReset: false,
-            timerInterval: 0
-        };
+  showZero = (currValue) => {
+    if (currValue < 10) {
+      return "0";
+    } else {
+      return "";
     }
+  };
 
-    showZero = currValue => {
-        if(currValue < 10){
-            return "0";
-        }else{
-            return "";
-        }
-    };
-
-    startTimer = () => {
-        let timerInterval = setInterval(()=>{
+  startTimer = () => {
+    let timerInterval = setInterval(() => {
+      this.setState(
+        {
+          timeInDeciSeconds: this.state.timeInDeciSeconds + 1,
+        },
+        () => {
+          if (this.state.timeInSeconds > 59) {
             this.setState({
-                timeInDeciSeconds : (this.state.timeInDeciSeconds + 1)
-            },()=>{
-                if(this.state.timeInSeconds > 59){
-                    this.setState({
-                        timeInDeciSeconds : 0,
-                        timeInSeconds: 0,
-                        timeInMinutes: (this.state.timeInMinutes + 1)
-                    });
-                }
-                if(this.state.timeInDeciSeconds >99){
-                    this.setState({
-                        timeInDeciSeconds : 0,
-                        timeInSeconds: (this.state.timeInSeconds + 1)
-                    });
-                }
+              timeInDeciSeconds: 0,
+              timeInSeconds: 0,
+              timeInMinutes: this.state.timeInMinutes + 1,
             });
-        },10);
+          }
+          if (this.state.timeInDeciSeconds > 99) {
+            this.setState({
+              timeInDeciSeconds: 0,
+              timeInSeconds: this.state.timeInSeconds + 1,
+            });
+          }
+        }
+      );
+    }, 10);
 
-        this.setState({
-            timerRunning : true
-        });
-        return timerInterval;
-    };
+    this.setState({
+      timerRunning: true,
+    });
+    return timerInterval;
+  };
 
-    pauseTimer = () => {
-        clearInterval(this.state.timerInterval);
-        this.setState({
-            timerRunning : false
-        });
-    };
+  pauseTimer = () => {
+    clearInterval(this.state.timerInterval);
+    this.setState({
+      timerRunning: false,
+    });
+  };
 
-    resetTimer = () => {
+  resetTimer = () => {
+    this.pauseTimer();
+    this.setState({
+      timeInDeciSeconds: 0,
+      timeInSeconds: 0,
+      timeInMinutes: 0,
+    });
+  };
+
+  showStartPause = () => {
+    if (this.state.timerRunning) {
+      return "Pause";
+    } else {
+      return "Start";
+    }
+  };
+
+  runStartPause = () => {
+    if (this.state.timerRunning) {
       this.pauseTimer();
-        this.setState({
-            timeInDeciSeconds : 0,
-            timeInSeconds: 0,
-            timeInMinutes: 0,
-        });
-    };
-
-    showStartPause = () => {
-        if(this.state.timerRunning){
-            return "Pause";
-        }else{
-            return "Start";
-        }
-    };
-
-    runStartPause = () => {
-        if(this.state.timerRunning){
-            this.pauseTimer();
-        }else{
-            this.setState({
-                timerInterval: this.startTimer()
-            });
-        }
-    };
-
-    render() {
-        return (
-            <div className="app-main">
-                <Display minZero = {this.showZero(this.state.timeInMinutes)}
-                         secZero = {this.showZero(this.state.timeInSeconds)}
-                         deciSecZero = {this.showZero(this.state.timeInDeciSeconds)}
-                         timeInMinutes = {this.state.timeInMinutes}
-                         timeInSeconds = {this.state.timeInSeconds}
-                         timeInDeciSeconds = {this.state.timeInDeciSeconds}
-                />
-                <div>
-                    <button onClick={this.runStartPause}>{this.showStartPause()} </button>
-                    <button onClick={this.resetTimer}> Reset</button>
-                </div>
-                <WebWorkerDemo/>
-            </div>
-        );
-
+    } else {
+      this.setState({
+        timerInterval: this.startTimer(),
+      });
     }
+  };
+
+  render() {
+    return (
+      <div className="app-main">
+        <Display
+          minZero={this.showZero(this.state.timeInMinutes)}
+          secZero={this.showZero(this.state.timeInSeconds)}
+          deciSecZero={this.showZero(this.state.timeInDeciSeconds)}
+          timeInMinutes={this.state.timeInMinutes}
+          timeInSeconds={this.state.timeInSeconds}
+          timeInDeciSeconds={this.state.timeInDeciSeconds}
+        />
+        <div>
+          <button onClick={this.runStartPause}>{this.showStartPause()}</button>
+          <button onClick={this.resetTimer}> Reset</button>
+        </div>
+        <WebWorkerDemo />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/WebWorkerDemo/WebWorkerDemo.js
+++ src/components/WebWorkerDemo/WebWorkerDemo.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "./styles.css";
+
+const incrementWithoutWorker = (score, setScore) => {
+  let toAdd = 0;
+  for (let i = 0; i < 10000; i++) {
+    for (let j = 0; j < 100000; j++) {
+      toAdd++;
+    }
+  }
+  setScore((score) => score + toAdd);
+};
 
 const WebWorkerDemo = () => {
+  const [score, setScore] = useState(0);
+
   return (
     <div className="web-worker-demo">
-      WebWorker
+      <div className="score">{score}</div>
+      <div className="demo-buttons">
+        <button onClick={() => incrementWithoutWorker(score, setScore)}>
+          Add in same thread
+        </button>
+      </div>
     </div>
-  )
+  );
 };
 
 export default WebWorkerDemo;
